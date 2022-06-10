67b9458be7ac950f6ce4ce370c83e0182de8893f
ian <sylvester16@gmail.com>
2022-06-10 15:39:09 -0400

    started adding styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import NavBtn from "./NavBtn";
 import React from "react";
+import Logo from "../images/marvel-us.jpg";
 const Header = (props) => {
   return (
     <div className="header">
-      <h1>header</h1>
-      <NavBtn text="Button 1" />
-      <NavBtn text="Button 2" />
-      <NavBtn text="Button 3" />
-      <NavBtn text="Button 4" />
-      <NavBtn text="Button 5" />
+      <div className="logo"><img src={Logo} /></div>
+      <div className="navLinks">
+        <NavBtn text="Button 1" />
+        <NavBtn text="Button 2" />
+        <NavBtn text="Button 3" />
+        <NavBtn text="Button 4" />
+        <NavBtn text="Button 5" />
+      </div>
     </div>
   );
 };
