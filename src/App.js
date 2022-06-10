fc0b75d8a1fa2eeb25a8b38f15a21ebd1bd4f3ea
ian <sylvester16@gmail.com>
2022-06-10 13:19:38 -0400

    created header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+const Header = (props)=>{
+    return(
+        <div className="header">
+            <h1>header</h1>
+        </div>
+    )
+}
+export default Header
